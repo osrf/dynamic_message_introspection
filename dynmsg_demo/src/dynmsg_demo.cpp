2005e1cfd8557701b8a3c4001b1fc8bafe37400e
Geoffrey Biggs <gbiggs@killbots.net>
2020-10-01 08:44:55 +0900

    Support nested messages (#1)
    * Refactor printing into a separate function
    
    Signed-off-by: Geoffrey Biggs <gbiggs@killbots.net>
    
    * Support nested messages
    
    Signed-off-by: Geoffrey Biggs <gbiggs@killbots.net>

--- dynmsg_demo/src/dynmsg_demo.cpp
+++ dynmsg_demo/src/dynmsg_demo.cpp
@@ -1,19 +1,27 @@
 #include <dlfcn.h>
-#include <sstream>
 #include <unistd.h>
 
+#include <iostream>
 #include <memory>
+#include <sstream>
 
-#include "rcl/rcl.h"
 #include "rcl/context.h"
 #include "rcl/init_options.h"
 #include "rcl/node_options.h"
 #include "rcl/node.h"
+#include "rcl/rcl.h"
 #include "rcl/subscription.h"
+#include "rcl/types.h"
 #include "rosidl_typesupport_introspection_c/message_introspection.h"
 #include "rosidl_typesupport_introspection_c/field_types.h"
 
 #include "rcutils/logging_macros.h"
+
+void message_to_string(
+  const rosidl_typesupport_introspection_c__MessageMembers * type_info,
+  std::shared_ptr<uint8_t> message_data,
+  size_t indent_level,
+  std::stringstream &output);
 
 
 const char *
@@ -165,6 +173,139 @@ member_value_to_string(
 }
 
 
+void add_indent(size_t indent_level, std::stringstream &output)
+{
+  for (size_t ii = 0; ii < indent_level; ++ii) {
+    output << "  ";
+  }
+}
+
+
+void member_to_string(
+  const rosidl_typesupport_introspection_c__MessageMember &member_info,
+  std::shared_ptr<uint8_t> message_data,
+  size_t indent_level,
+  std::stringstream &output)
+{
+  if (member_info.type_id_ == rosidl_typesupport_introspection_c__ROS_TYPE_MESSAGE) {
+    // Nested message member
+    const rosidl_typesupport_introspection_c__MessageMembers * nested_type_info =
+      reinterpret_cast<const rosidl_typesupport_introspection_c__MessageMembers *>(
+        member_info.members_->data);
+    add_indent(indent_level, output);
+    output << member_info.name_ << " (" << nested_type_info->message_namespace_ <<
+      '/' << nested_type_info->message_name_ << "):\n";
+    message_to_string(
+      nested_type_info,
+      std::shared_ptr<uint8_t>(message_data, message_data.get() + member_info.offset_),
+      indent_level + 1,
+      output);
+  } else {
+    // POD member
+    add_indent(indent_level, output);
+    output << member_info.name_ << " (" <<
+      member_type_to_string(member_info) << "):\t" <<
+      member_value_to_string(member_info, message_data) << '\n';
+  }
+}
+
+
+void message_to_string(
+  const rosidl_typesupport_introspection_c__MessageMembers * type_info,
+  std::shared_ptr<uint8_t> message_data,
+  size_t indent_level,
+  std::stringstream &output)
+{
+  for (uint32_t ii = 0; ii < type_info->member_count_; ++ii) {
+    member_to_string(type_info->members_[ii], message_data, indent_level, output);
+  }
+}
+
+
+typedef struct TypeSupport
+{
+  void * type_support_library;
+  const rosidl_message_type_support_t * type_support;
+  void * introspection_library;
+  const rosidl_message_type_support_t * introspection;
+  const rosidl_typesupport_introspection_c__MessageMembers * type_info;
+} TypeSupport;
+
+
+rcl_ret_t load_interface_type_support(
+  const std::string &namespace_,
+  const std::string &interface,
+  TypeSupport &type_support)
+{
+  std::string ts_lib_name;
+  ts_lib_name = "lib" + namespace_ + "__rosidl_typesupport_c.so";
+  RCUTILS_LOG_DEBUG_NAMED("dynmsg_demo", "Loading type support library %s", ts_lib_name.c_str());
+  type_support.type_support_library = dlopen(ts_lib_name.c_str(), RTLD_LAZY);
+  if (type_support.type_support_library == nullptr) {
+    RCUTILS_LOG_ERROR_NAMED("dynmsg_demo", "failed to load type support library: %s", dlerror());
+    return RCL_RET_ERROR;
+  }
+  std::string ts_func_name;
+  ts_func_name = "rosidl_typesupport_c__get_message_type_support_handle__" + namespace_ +
+    "__msg__" + interface;
+  RCUTILS_LOG_DEBUG_NAMED(
+    "dynmsg_demo",
+    "Loading type support function %s",
+    ts_func_name.c_str());
+  typedef const rosidl_message_type_support_t* (*get_interface_ts_func)();
+  get_interface_ts_func type_support_handle_func = reinterpret_cast<get_interface_ts_func>(dlsym(
+    type_support.type_support_library,
+    ts_func_name.c_str()));
+  if (type_support_handle_func == nullptr) {
+    RCUTILS_LOG_ERROR_NAMED("dynmsg_demo", "failed to load type support function: %s", dlerror());
+    return RCL_RET_ERROR;
+  }
+  type_support.type_support = type_support_handle_func();
+  RCUTILS_LOG_DEBUG_NAMED(
+    "dynmsg_demo",
+    "Loaded type support %s",
+    type_support.type_support->typesupport_identifier);
+
+  ts_lib_name = "lib" + namespace_ + "__rosidl_typesupport_introspection_c.so";
+  RCUTILS_LOG_DEBUG_NAMED(
+    "dynmsg_demo",
+    "Loading introspection type support library %s",
+    ts_lib_name.c_str());
+  type_support.introspection_library = dlopen(ts_lib_name.c_str(), RTLD_LAZY);
+  if (type_support.introspection_library == nullptr) {
+    RCUTILS_LOG_ERROR_NAMED(
+      "dynmsg_demo",
+      "failed to load introspection type support library: %s",
+      dlerror());
+    return RCL_RET_ERROR;
+  }
+  ts_func_name = "rosidl_typesupport_introspection_c__get_message_type_support_handle__" +
+    namespace_ + "__msg__" + interface;
+  RCUTILS_LOG_DEBUG_NAMED("dynmsg_demo", "Loading type support function %s", ts_func_name.c_str());
+  get_interface_ts_func introspection_type_support_handle_func =
+    reinterpret_cast<get_interface_ts_func>(dlsym(
+      type_support.introspection_library,
+      ts_func_name.c_str()));
+  if (type_support.introspection_library == nullptr) {
+    RCUTILS_LOG_ERROR_NAMED(
+      "dynmsg_demo",
+      "failed to load introspection type support function: %s",
+      dlerror());
+    return RCL_RET_ERROR;
+  }
+  type_support.introspection = introspection_type_support_handle_func();
+  RCUTILS_LOG_DEBUG_NAMED(
+    "dynmsg_demo",
+    "Loaded type support %s",
+    type_support.introspection->typesupport_identifier);
+  type_support.type_info =
+    reinterpret_cast<const rosidl_typesupport_introspection_c__MessageMembers *>(
+      type_support.introspection->data);
+
+  return RCL_RET_OK;
+}
+
+
 int
 main(int argc, char ** argv) {
   std::string msg_namespace(argv[1]);
@@ -189,7 +330,7 @@ main(int argc, char ** argv) {
     return 1;
   }
 
-  RCUTILS_LOG_INFO_NAMED("dynmsg_demo", "Creating node");
+  RCUTILS_LOG_DEBUG_NAMED("dynmsg_demo", "Creating node");
   rcl_node_options_t node_options = rcl_node_get_default_options();
   rcl_node_t node = rcl_get_zero_initialized_node();
   ret = rcl_node_init(&node, "dynmsg_test", "", &context, &node_options);
@@ -198,98 +339,46 @@ main(int argc, char ** argv) {
     return 1;
   }
 
-  std::stringstream ts_lib_name;
-  ts_lib_name << "lib" << msg_namespace << "__rosidl_typesupport_c.so";
-  RCUTILS_LOG_INFO_NAMED("dynmsg_demo", "Loading type support library %s", ts_lib_name.str().c_str());
-  void * type_support_lib = dlopen(ts_lib_name.str().c_str(), RTLD_LAZY);
-  if (type_support_lib == nullptr) {
-    RCUTILS_LOG_ERROR_NAMED("dynmsg_demo", "failed to load type support library: %s", dlerror());
+  TypeSupport type_support;
+  if (load_interface_type_support(msg_namespace, msg_type, type_support) != RCL_RET_OK) {
+    // Error message already printed
     return 1;
   }
-  typedef const rosidl_message_type_support_t* (*get_message_ts_func)();
-  std::stringstream ts_func_name;
-  ts_func_name << "rosidl_typesupport_c__get_message_type_support_handle__" << msg_namespace <<
-    "__msg__" << msg_type;
-  RCUTILS_LOG_INFO_NAMED("dynmsg_demo", "Loading type support function %s", ts_func_name.str().c_str());
-  get_message_ts_func type_support_handle_func = reinterpret_cast<get_message_ts_func>(dlsym(
-    type_support_lib,
-    ts_func_name.str().c_str()));
-  if (type_support_handle_func == nullptr) {
-    RCUTILS_LOG_ERROR_NAMED("dynmsg_demo", "failed to load type support function: %s", dlerror());
-    return 1;
-  }
-  const rosidl_message_type_support_t *ts = type_support_handle_func();
-  RCUTILS_LOG_INFO_NAMED(
-    "dynmsg_demo",
-    "Loaded type support %s",
-    ts->typesupport_identifier);
 
-  ts_lib_name.str(std::string());
-  ts_lib_name << "lib" << msg_namespace << "__rosidl_typesupport_introspection_c.so";
-  RCUTILS_LOG_INFO_NAMED(
-    "dynmsg_demo",
-    "Loading introspection type support library %s",
-    ts_lib_name.str().c_str());
-  void * introspection_type_support_lib = dlopen(ts_lib_name.str().c_str(), RTLD_LAZY);
-  if (introspection_type_support_lib == nullptr) {
-    RCUTILS_LOG_ERROR_NAMED("dynmsg_demo", "failed to load introspection type support library: %s", dlerror());
-    return 1;
-  }
-  ts_func_name.str(std::string());
-  ts_func_name << "rosidl_typesupport_introspection_c__get_message_type_support_handle__" << msg_namespace <<
-    "__msg__" << msg_type;
-  RCUTILS_LOG_INFO_NAMED("dynmsg_demo", "Loading type support function %s", ts_func_name.str().c_str());
-  get_message_ts_func introspection_type_support_handle_func =
-    reinterpret_cast<get_message_ts_func>(dlsym(
-      introspection_type_support_lib,
-      ts_func_name.str().c_str()));
-  if (introspection_type_support_handle_func == nullptr) {
-    RCUTILS_LOG_ERROR_NAMED(
-      "dynmsg_demo",
-      "failed to load introspection type support function: %s",
-      dlerror());
-    return 1;
-  }
-  const rosidl_message_type_support_t *introspection_ts = introspection_type_support_handle_func();
-  RCUTILS_LOG_INFO_NAMED(
-    "dynmsg_demo",
-    "Loaded type support %s",
-    introspection_ts->typesupport_identifier);
-  const rosidl_typesupport_introspection_c__MessageMembers * type_info =
-    reinterpret_cast<const rosidl_typesupport_introspection_c__MessageMembers *>(introspection_ts->data);
-
-  RCUTILS_LOG_INFO_NAMED("dynmsg_demo", "Creating subscription");
+  RCUTILS_LOG_DEBUG_NAMED("dynmsg_demo", "Creating subscription");
   rcl_subscription_t sub = rcl_get_zero_initialized_subscription();
   rcl_subscription_options_t sub_options = rcl_subscription_get_default_options();
-  ret = rcl_subscription_init(&sub, &node, ts, "dynmsg", &sub_options);
+  ret = rcl_subscription_init(&sub, &node, type_support.type_support, "dynmsg", &sub_options);
   if (ret != RCL_RET_OK) {
     RCUTILS_LOG_ERROR_NAMED("dynmsg_demo", "subscription init failed");
     return 1;
   }
 
   bool running = true;
-  while(running) {
+  while (running) {
     size_t count{0};
     ret = rmw_subscription_count_matched_publishers(rcl_subscription_get_rmw_handle(&sub), &count);
     if (ret != RCL_RET_OK) {
       RCUTILS_LOG_ERROR_NAMED("dynmsg_demo", "pub count failed");
       return 1;
     }
-    RCUTILS_LOG_INFO_NAMED("dynmsg_demo", "There are %ld matched publishers", count);
+    RCUTILS_LOG_DEBUG_NAMED("dynmsg_demo", "There are %ld matched publishers", count);
     if (count > 0) {
       break;
     }
     sleep(0.25);
   }
 
-  RCUTILS_LOG_INFO_NAMED(
+  RCUTILS_LOG_DEBUG_NAMED(
     "dynmsg_demo",
     "Allocating message buffer of size %ld bytes",
-    type_info->size_of_);
-  std::shared_ptr<uint8_t> message(new uint8_t[type_info->size_of_]);
+    type_support.type_info->size_of_);
+  std::shared_ptr<uint8_t> message(
+    new uint8_t[type_support.type_info->size_of_],
+    std::default_delete<uint8_t[]>());
 
   bool taken = false;
-  while(!taken) {
+  while (!taken) {
     taken = false;
     ret = rmw_take(rcl_subscription_get_rmw_handle(&sub), message.get(), &taken, nullptr);
     if (ret != RCL_RET_OK) {
@@ -297,26 +386,16 @@ main(int argc, char ** argv) {
       return 1;
     }
     if (taken) {
-      RCUTILS_LOG_INFO_NAMED("dynmsg_demo", "Received data");
+      RCUTILS_LOG_DEBUG_NAMED("dynmsg_demo", "Received data");
       break;
     }
   }
 
-  // Iterate over the message members
-  RCUTILS_LOG_INFO_NAMED(
-    "dynmsg_demo",
-    "Received message is of type %s/%s",
-    type_info->message_namespace_,
-    type_info->message_name_);
-  RCUTILS_LOG_INFO_NAMED("dynmsg_demo", "Message field values:");
-  for (uint32_t ii = 0; ii < type_info->member_count_; ++ii) {
-    RCUTILS_LOG_INFO_NAMED(
-      "dynmsg_demo",
-      "  %s (%s):\t%s",
-      type_info->members_[ii].name_,
-      member_type_to_string(type_info->members_[ii]),
-      member_value_to_string(type_info->members_[ii], message).c_str());
-  }
+  std::stringstream output;
+  output << type_support.type_info->message_namespace_ << '/' <<
+    type_support.type_info->message_name_ << '\n';
+  message_to_string(type_support.type_info, message, 1, output);
+  std::cout << output.str();
 
   ret = rcl_node_fini(&node);
   if (ret != RCL_RET_OK) {
